7bce3db3821a3175b67e189e0d98772d00e8e967
Rafaelmorales1982 <rafaelmorconcursos@gmail.com>
2026-01-21 16:42:00 -0300

    Atualizando tipos de variáveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -2,7 +2,7 @@ public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
         double salario = 5500.00;
         short numeroCurto = 1;
-        int numeroNormal = numeroCurto; 
+        int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
         // declarando uma variável constante - usar "final" e sempre nome seja em maiúsculo
         final double VALOR_DE_PI = 3.14;
@@ -18,6 +18,7 @@ public class TiposVariaveis {
         System.out.println(numeroCurto2);
         System.out.println(salario);
         System.out.println(VALOR_DE_PI);
+        System.out.println(meuNome );
 
     }
 }
